47823194f54200df2461a4690035dcf6d9f7109f
jonesjacklewis <jones.jack.lewis@gmail.com>
2025-12-19 14:54:29 +0000

    Added clothing struct and tests

--- internal/domain/clothing.go
+++ internal/domain/clothing.go
@@ -1,1 +1,44 @@
 package domain
+
+import (
+	"errors"
+	"strings"
+)
+
+type Clothing struct {
+	ClothingType string `json:"clothingType"`
+	Description  string `json:"description"`
+	Brand        string `json:"brand"`
+	Store        string `json:"store"`
+	ImageUrl     string `json:"imageUrl"`
+	Price        Pence  `json:"pricePence"`
+	Size         string `json:"size"`
+}
+
+func (c Clothing) IsValidItem() error {
+	if int(c.Price) < 0 {
+		return errors.New("Clothing Price must be greater than or equal to 0")
+	}
+
+	if strings.TrimSpace(c.ClothingType) == "" {
+		return errors.New("Clothing Type must not be empty")
+	}
+
+	if strings.TrimSpace(c.Description) == "" {
+		return errors.New("Clothing Description must not be empty")
+	}
+
+	if strings.TrimSpace(c.Brand) == "" {
+		return errors.New("Clothing Brand must not be empty")
+	}
+
+	if strings.TrimSpace(c.Store) == "" {
+		return errors.New("Clothing Store must not be empty")
+	}
+
+	if strings.TrimSpace(c.Size) == "" {
+		return errors.New("Clothing Size must not be empty")
+	}
+
+	return nil
+}
--- internal/domain/clothing_test.go
+++ internal/domain/clothing_test.go
@@ -1,1 +1,298 @@
 package domain
+
+import "testing"
+
+func TestIsValid(t *testing.T) {
+	// Happy Path
+	t.Run("Given Clothing has positive Price, and not empty ClothingType, Description, Brand, Store, and Size - Then should return nil", func(t *testing.T) {
+		var item Clothing = Clothing{
+			Price:        Pence(2000),
+			ClothingType: "Jumper",
+			Description:  "Red Loosefit Jumper",
+			Brand:        "A&B",
+			Store:        "Totally Real Store",
+			Size:         "Medium",
+		}
+		got := item.IsValidItem()
+
+		if got != nil {
+			t.Errorf("Expected no error, but got %v", got)
+		}
+	})
+
+	// Negative Price
+
+	t.Run("Given Clothing has negative Price, should return an appropriate error", func(t *testing.T) {
+		var item Clothing = Clothing{
+			Price:        Pence(-2000),
+			ClothingType: "Jumper",
+			Description:  "Red Loosefit Jumper",
+			Brand:        "A&B",
+			Store:        "Totally Real Store",
+			Size:         "Medium",
+		}
+
+		got := item.IsValidItem()
+
+		if got == nil {
+			t.Error("Expected error, but got nil")
+		}
+
+		text := got.Error()
+		expectedText := "Clothing Price must be greater than or equal to 0"
+
+		if expectedText != text {
+			t.Errorf("Expected '%s' but got '%s'", expectedText, text)
+		}
+	})
+
+	// Empty/Whitespace ClothingType
+
+	t.Run("Given Clothing has empty ClothingType, should return an appropriate error", func(t *testing.T) {
+		var item Clothing = Clothing{
+			Price:        Pence(2000),
+			ClothingType: "",
+			Description:  "Red Loosefit Jumper",
+			Brand:        "A&B",
+			Store:        "Totally Real Store",
+			Size:         "Medium",
+		}
+
+		got := item.IsValidItem()
+
+		if got == nil {
+			t.Error("Expected error, but got nil")
+		}
+
+		text := got.Error()
+		expectedText := "Clothing Type must not be empty"
+
+		if expectedText != text {
+			t.Errorf("Expected '%s' but got '%s'", expectedText, text)
+		}
+	})
+
+	t.Run("Given Clothing has whitespace ClothingType, should return an appropriate error", func(t *testing.T) {
+		var item Clothing = Clothing{
+			Price:        Pence(2000),
+			ClothingType: "   ",
+			Description:  "Red Loosefit Jumper",
+			Brand:        "A&B",
+			Store:        "Totally Real Store",
+			Size:         "Medium",
+		}
+
+		got := item.IsValidItem()
+
+		if got == nil {
+			t.Error("Expected error, but got nil")
+		}
+
+		text := got.Error()
+		expectedText := "Clothing Type must not be empty"
+
+		if expectedText != text {
+			t.Errorf("Expected '%s' but got '%s'", expectedText, text)
+		}
+	})
+
+	// Empty/Whitespace Description
+
+	t.Run("Given Clothing has empty Description, should return an appropriate error", func(t *testing.T) {
+		var item Clothing = Clothing{
+			Price:        Pence(2000),
+			ClothingType: "Jumper",
+			Description:  "",
+			Brand:        "A&B",
+			Store:        "Totally Real Store",
+			Size:         "Medium",
+		}
+
+		got := item.IsValidItem()
+
+		if got == nil {
+			t.Error("Expected error, but got nil")
+		}
+
+		text := got.Error()
+		expectedText := "Clothing Description must not be empty"
+
+		if expectedText != text {
+			t.Errorf("Expected '%s' but got '%s'", expectedText, text)
+		}
+	})
+
+	t.Run("Given Clothing has whitespace Description, should return an appropriate error", func(t *testing.T) {
+		var item Clothing = Clothing{
+			Price:        Pence(2000),
+			ClothingType: "Jumper",
+			Description:  " ",
+			Brand:        "A&B",
+			Store:        "Totally Real Store",
+			Size:         "Medium",
+		}
+
+		got := item.IsValidItem()
+
+		if got == nil {
+			t.Error("Expected error, but got nil")
+		}
+
+		text := got.Error()
+		expectedText := "Clothing Description must not be empty"
+
+		if expectedText != text {
+			t.Errorf("Expected '%s' but got '%s'", expectedText, text)
+		}
+	})
+
+	// Empty/Whitespace Brand
+
+	t.Run("Given Clothing has empty Brand, should return an appropriate error", func(t *testing.T) {
+		var item Clothing = Clothing{
+			Price:        Pence(2000),
+			ClothingType: "Jumper",
+			Description:  "Red Loosefit Jumper",
+			Brand:        "",
+			Store:        "Totally Real Store",
+			Size:         "Medium",
+		}
+
+		got := item.IsValidItem()
+
+		if got == nil {
+			t.Error("Expected error, but got nil")
+		}
+
+		text := got.Error()
+		expectedText := "Clothing Brand must not be empty"
+
+		if expectedText != text {
+			t.Errorf("Expected '%s' but got '%s'", expectedText, text)
+		}
+	})
+
+	t.Run("Given Clothing has whitespace Brand, should return an appropriate error", func(t *testing.T) {
+		var item Clothing = Clothing{
+			Price:        Pence(2000),
+			ClothingType: "Jumper",
+			Description:  "Red Loosefit Jumper",
+			Brand:        " ",
+			Store:        "Totally Real Store",
+			Size:         "Medium",
+		}
+
+		got := item.IsValidItem()
+
+		if got == nil {
+			t.Error("Expected error, but got nil")
+		}
+
+		text := got.Error()
+		expectedText := "Clothing Brand must not be empty"
+
+		if expectedText != text {
+			t.Errorf("Expected '%s' but got '%s'", expectedText, text)
+		}
+	})
+
+	// Empty/Whitespace Store
+
+	t.Run("Given Clothing has empty Store, should return an appropriate error", func(t *testing.T) {
+		var item Clothing = Clothing{
+			Price:        Pence(2000),
+			ClothingType: "Jumper",
+			Description:  "Red Loosefit Jumper",
+			Brand:        "A&B",
+			Store:        "",
+			Size:         "Medium",
+		}
+
+		got := item.IsValidItem()
+
+		if got == nil {
+			t.Error("Expected error, but got nil")
+		}
+
+		text := got.Error()
+		expectedText := "Clothing Store must not be empty"
+
+		if expectedText != text {
+			t.Errorf("Expected '%s' but got '%s'", expectedText, text)
+		}
+	})
+
+	t.Run("Given Clothing has whitespace Store, should return an appropriate error", func(t *testing.T) {
+		var item Clothing = Clothing{
+			Price:        Pence(2000),
+			ClothingType: "Jumper",
+			Description:  "Red Loosefit Jumper",
+			Brand:        "A&B",
+			Store:        " ",
+			Size:         "Medium",
+		}
+
+		got := item.IsValidItem()
+
+		if got == nil {
+			t.Error("Expected error, but got nil")
+		}
+
+		text := got.Error()
+		expectedText := "Clothing Store must not be empty"
+
+		if expectedText != text {
+			t.Errorf("Expected '%s' but got '%s'", expectedText, text)
+		}
+	})
+
+	// Empty/Whitespace Size
+
+	t.Run("Given Clothing has empty Size, should return an appropriate error", func(t *testing.T) {
+		var item Clothing = Clothing{
+			Price:        Pence(2000),
+			ClothingType: "Jumper",
+			Description:  "Red Loosefit Jumper",
+			Brand:        "A&B",
+			Store:        "Totally Real Store",
+			Size:         "",
+		}
+
+		got := item.IsValidItem()
+
+		if got == nil {
+			t.Error("Expected error, but got nil")
+		}
+
+		text := got.Error()
+		expectedText := "Clothing Size must not be empty"
+
+		if expectedText != text {
+			t.Errorf("Expected '%s' but got '%s'", expectedText, text)
+		}
+	})
+
+	t.Run("Given Clothing has whitespace Size, should return an appropriate error", func(t *testing.T) {
+		var item Clothing = Clothing{
+			Price:        Pence(2000),
+			ClothingType: "Jumper",
+			Description:  "Red Loosefit Jumper",
+			Brand:        "A&B",
+			Store:        "Totally Real Store",
+			Size:         " ",
+		}
+
+		got := item.IsValidItem()
+
+		if got == nil {
+			t.Error("Expected error, but got nil")
+		}
+
+		text := got.Error()
+		expectedText := "Clothing Size must not be empty"
+
+		if expectedText != text {
+			t.Errorf("Expected '%s' but got '%s'", expectedText, text)
+		}
+	})
+}
